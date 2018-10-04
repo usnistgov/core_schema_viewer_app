d77f494022e5421702e7a16382c634d662848b48
Adrien Catel <adrien.catel@nist.gov>
2018-10-04 15:31:55 -0400

    fix: when core_schema_viewer_app is added to an existing project. a TemplateViewerSchema is created for each existing template in db
    Change-Id: If8d25b8c253d4937e87b0caf9613b17b314deded

--- core_schema_viewer_app/components/template_schema_viewer/watch.py
+++ core_schema_viewer_app/components/template_schema_viewer/watch.py
@@ -2,15 +2,17 @@
 """
 from core_main_app.commons import exceptions
 from core_main_app.components.template.models import Template
+from core_main_app.components.template  import api as template_api
 from core_schema_viewer_app.components.template_schema_viewer import api as template_schema_viewer_api
 from core_schema_viewer_app.components.template_schema_viewer.models import TemplateSchemaViewer
 from signals_utils.signals.mongo import connector, signals
 
 
 def init():
-    """ Connect to Template object events.
+    """ Connect to Template object events and create template schema viewer for existing templates
     """
     connector.connect(post_save_template, signals.post_save, Template)
+    create_template_schema_viewer_from_templates_in_db()
 
 
 def post_save_template(sender, document, **kwargs):
@@ -27,3 +29,18 @@ def post_save_template(sender, document, **kwargs):
         # upsert if does not exist for this template
         template_schema_viewer = TemplateSchemaViewer(template=document)
         return template_schema_viewer_api.upsert(template_schema_viewer)
+
+
+def create_template_schema_viewer_from_templates_in_db():
+    """ Get all template in DB and create a TemplateSchemaViewer for each
+    """
+    # get all template in DB
+    templates = template_api.get_all()
+    for template in templates:
+        try:
+            # check if the TemplateSchemaViewer does not exist already
+            template_schema_viewer_api.get_by_template_id(template.pk)
+        except exceptions.DoesNotExist:
+            # create the TemplateSchemaViewer associated only if it does not exist already
+            template_schema_viewer = TemplateSchemaViewer(template=template)
+            template_schema_viewer_api.upsert(template_schema_viewer)
