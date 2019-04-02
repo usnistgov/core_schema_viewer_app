94cd3de2dc9032048db3129a8db9e97563a5e160
Yande <ndeye.ndiaye@nist.gov>
2019-04-02 15:22:51 -0400

    fix: Update images, add menu name, change order of sandbox view & tabbed view
    Change-Id: I9cb947425af26bbf6c7ce6af1da44a1a8b6b23a8

--- core_schema_viewer_app/menus.py
+++ core_schema_viewer_app/menus.py
@@ -2,9 +2,10 @@
 """
 from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
+from core_schema_viewer_app.settings import SCHEMA_VIEWER_MENU_NAME
 
 Menu.add_item(
-    "main", MenuItem("Schema Viewer", reverse("core_schema_viewer_index"))
+    "main", MenuItem(SCHEMA_VIEWER_MENU_NAME, reverse("core_schema_viewer_index"))
 )
 
 schema_viewer_children = (
